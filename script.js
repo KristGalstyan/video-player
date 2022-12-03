b19e961907f35aa663247492bcc144944c86b310
Krist <kitog@bk.ru>
2022-12-03 21:30:39 +0400

    update the current time as the video is playing

--- script.js
+++ script.js
@@ -34,9 +34,19 @@ video.addEventListener('ended', showPlayIcon);
 
 // Progress Bar ---------------------------------- //
 
+// Calculate display time format
+function displayTime(time) {
+    const minutes = Math.floor(time / 60);
+    let seconds = Math.floor(time % 60);
+    seconds = seconds > 9 ? seconds : `0${seconds}`;
+    return `${minutes}:${seconds}`;
+}
+
 // Update Progress Bar as video plays
 function upDateProgress() {
     progressBar.style.width = `${(video.currentTime / video.duration) * 100}%`;
+    currentTime.textContent = `${displayTime(video.currentTime)} /`;
+    durration.textContent = `${displayTime(video.duration)}`;
 }
 
 
