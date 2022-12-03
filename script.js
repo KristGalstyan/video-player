34d8042e737d91f67b44f17bd64f77d5ba05f13a
Krist <kitog@bk.ru>
2022-12-03 21:02:03 +0400

    Play & Pause Event

--- script.js
+++ script.js
@@ -1,8 +1,36 @@
+const video = document.querySelector('video'),
+      progressRange = document.querySelector('.progress-range'),
+      progressBar = document.querySelector('.progress-bar'),
+      playBtn = document.querySelector('#play-btn'),
+      volumeIcon = document.querySelector('#volume-icon'),
+      volumeRange = document.querySelector('.volume-range'),
+      volumeBar = document.querySelector('.volume-bar'),
+      currentTime = document.querySelector('.time-elapsed'),
+      durration = document.querySelector('.time-duration'),
+      fullscreen = document.querySelector('.fullscreen');
+
 
 
 // Play & Pause ----------------------------------- //
 
+function showPlayIcon() {
+    playBtn.classList.replace('fa-pause', 'fa-play');
+    playBtn.setAttribute('title', 'Play');
+}
 
+function togglePlay() {
+    if (video.paused) {
+        video.play();
+        playBtn.classList.replace('fa-play', 'fa-pause');
+        playBtn.setAttribute('title', 'Pause');
+    } else {
+        video.pause();
+        showPlayIcon();
+    }
+}
+
+// On Video End, show play button icon
+video.addEventListener('ended', showPlayIcon);
 
 // Progress Bar ---------------------------------- //
 
@@ -19,3 +47,6 @@
 // Fullscreen ------------------------------- //
 
 
+// EventListener
+playBtn.addEventListener('click', togglePlay);
+video.addEventListener('click', togglePlay);
